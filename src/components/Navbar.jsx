a5dfa5ade7961571eeb53113a43b3cb443306b15
Shajwan <shajwannamiqsalh@gmail.com>
2022-08-05 14:34:50 +0300

    initial commit 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {
 
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { BsBehance, BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/Logo.png';
 import {Link} from 'react-scroll';
 
@@ -91,31 +91,41 @@ const Navbar = () => {
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className='w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#457b9d]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/shajwan-namiq-5bb90020b/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+          <li className='w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#292828]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Shajwan-Namiq'
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+          <li className='w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ae251b]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZXhHDFCpHCxssgZGcvGCFbPjtRcdzDhCQMzBrjzbPfBVvBWzGqDghnrVzfNgPcCQmcBML'
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+
+<li className='w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1d1bae]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.behance.net/shajwannamiq'
+            >
+              Behance <BsBehance size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#4a7649]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
               href='/'
